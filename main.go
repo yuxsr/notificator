6e1d1d934a8440ff6c35d186f6a7fdf87af99382
Yushi Abe <handballer006@gmail.com>
2023-09-29 13:02:22 +0900

    :recycle:

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"log/slog"
-	"os"
-
 	"github.com/yuxsr/notificator/cmd"
+	"github.com/yuxsr/notificator/logging"
 )
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	rootCmd := cmd.NewRootCmd()
-	if err := rootCmd.Execute(); err != nil {
+	logger := logging.NewLogger()
+	if err := cmd.NewRootCmd().Execute(); err != nil {
 		logger.Error("command execution error", "error", err)
 	}
 }
