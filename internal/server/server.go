332ce81691829f7217d792bf84dce13721bc2d4c
yushi-abe <handballer006@gmail.com>
2025-07-05 22:23:12 +0900

    :arrow_up: update proto

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,13 +2,13 @@ package server
 
 import (
 	"github.com/yuxsr/notificator/internal/server/service"
-	protov1 "github.com/yuxsr/yuxsr-dev-pb/gencode/go/proto/v1"
+	yuxsrdevpbv1 "github.com/yuxsr/yuxsr-dev-pb/gencode/go/yuxsr_dev_pb/v1"
 	"google.golang.org/grpc"
 )
 
 func RegisterNewGRPCServer(config service.NotificatorServiceConfig) *grpc.Server {
 	notificatorService := service.NewNotificatorService(config)
 	s := grpc.NewServer()
-	protov1.RegisterNotificatorServiceServer(s, notificatorService)
+	yuxsrdevpbv1.RegisterNotificatorServiceServer(s, notificatorService)
 	return s
 }
